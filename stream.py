9929dc80cc28e495563ae1ce3d31ed3414d4a645
itisaknife234 <57622688+itisaknife234@users.noreply.github.com>
2025-03-06 09:53:14 +0900

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,19 @@
+#사용하려면 해당 디렉터리로 이동뒤
+#streamlit run stream.py
+
+import streamlit as st
+import datetime
+import pyupbit
+
+d = st.date_input (
+    "날짜를 선택 : ", datetime.date.today())
+
+st.write('비트코인 1일 차트')
+
+ticker = 'KRW-BTC'
+interval = 'minute60'
+to = str(d + datetime.timedelta(days=1))
+count = 24
+price_now = pyupbit.get_ohlcv(ticker=ticker, interval=interval, to=to,count=count)
+
+st.line_chart(price_now.close)
